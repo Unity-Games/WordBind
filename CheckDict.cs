359e9d85cd728e0918de2fbb96526c8c29c913f5
Souvik Datta <35172112+Souvik7cd@users.noreply.github.com>
2018-02-09 08:04:42 +0530

    Add files via upload

---
+++ CheckDict.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MyTree;
+
+public class CheckDict : MonoBehaviour {
+
+	public InputField inpText;
+	public Text showText;
+	public TextAsset dictFile;
+
+	private string textToString;
+
+	private List<string> dictList;
+	private int dictLen;
+	private PrefixTree dictionary;
+
+	void Start () {
+		dictList = TextToList (dictFile);
+		dictLen = dictList.Count;
+		dictionary = new PrefixTree ();
+		//dictionary.root = new Node();
+
+		showText.text = dictList[5];
+		for (int i = 0; i < 20; i++)
+			dictionary.Insert (dictList [i]);
+	}
+
+	private List<string> TextToList (TextAsset ts) {
+		return new List<string> (ts.text.Split ('\n'));
+	}
+	public void OnSubmit () {
+		textToString = inpText.text.ToString();
+		int i = dictionary.Search (textToString);
+		showText.text = i.ToString();
+//		if (dictList.BinarySearch (textToString) < 0)
+//			showText.text = "Word missing from dictionary";
+//		else
+//			showText.text = textToString;
+	}
+}
---
+++ PrefixTree.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyTree {
+	public class Node {
+		const int ALPHABET_SIZE = 26;
+		// declaring 26 child nodes
+		public Node[] children = new Node[ALPHABET_SIZE];
+		// variable to check if at that node a word ends or not
+		public bool isEndOfWord;
+		// Node constructor
+		public Node() {
+			isEndOfWord = false;
+			// initialsing all child nodes with null value
+			for (int i = 0; i < ALPHABET_SIZE; i++)
+				children[i]=(null);
+		}
+	}
+	public class PrefixTree {
+		public Node root;
+		public PrefixTree () {
+			root = getNode();
+		}
+
+		public Node getNode () {
+			return new Node ();
+		}
+
+		public void Insert (string key) {
+			int level;
+			int length = key.Length;
+			int i;
+
+			// making a root
+			Node pCrawl = root;
+			// iterating from the first letter to the last letter
+			for (level = 0; level < length; level++) {
+				i = key[level] - 'a';
+				if (pCrawl.children[i] == null)
+					pCrawl.children[i] = getNode();
+				pCrawl = pCrawl.children[i];
+			}
+			pCrawl.isEndOfWord = true;
+		}
+
+		public int Search(string key) {
+			int level;
+			int length = key.Length;
+			int i;
+			Node pCrawl = root;
+
+			for (level = 0; level < length; level++) {
+				i = key[level] - 'a';
+
+				if (pCrawl.children[i] == null)
+					return 0;	//Absent
+				pCrawl = pCrawl.children[i];
+			}
+			if (pCrawl != null) {
+				if (pCrawl.isEndOfWord == true)
+					return 1;	//Present
+				return 2;		//Can be completed
+			}
+			else
+				return 0;
+		}
+	}
+}
